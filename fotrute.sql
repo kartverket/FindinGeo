81c9b8e9c19e64cd55efb23df6245e2ab0fe4372
Christian Aron Vea <christian.vea@kartverket.no>
2025-03-18 10:25:34 +0100

    lagd spørsmål til fotrute

--- fotrute.sql
+++ fotrute.sql
@@ -26,3 +26,36 @@ SELECT objektid, datafangstdato
 FROM tur_og_friluftsruter.fotrute
 ORDER BY datafangstdato ASC;
 LIMIT 50;
+
+
+"Hvilken fotrute ligger lengst vest?"
+
+
+"Når var siste oppdaterte fotrute?"
+
+
+"Hvor mange fotruter i Ås er lengre enn 10 km?"
+
+
+"Hvor mange kilometer med fotrute er det i Ås?"
+
+
+"Hva er tettheten av fotruter i Ås?"
+"Hvor mange fotruter er det i Ås?"
+
+
+"Hvor mange kilometer er traktorveg i Ås?"
+
+
+"Hvor mange av fotrutene er frihåndstegning på skjerm?"
+
+"Hvilken type fotrute er det flest av i Ås?"
+
+"Hva er den lengste fotruten i Ås?"
+
+"Hvilke fotruter er kartlagt med en nøyaktighet på under 1 meter?"
+
+
+"Hvilke målemetoder er brukt for å kartlegge fotruter i Ås?"
+
+"I hvilket år ble det registrert flest fotruter?"
