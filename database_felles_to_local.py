5d9fb16fe3974bf866e543ae5451e6af1b032819
Harri <harrishan.jegatheeswaran@kartverket.no>
2025-04-08 11:03:09 +0200

    small changes

--- database_felles_to_local.py
+++ database_felles_to_local.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 
 # --- CONFIGURATION SETTINGS ---
-SQL_FILE_NAME = "dataset_querys/fotrute.sql" # SQL file with a query to execute
-TABLE_NAME = "fotrute_aas"
+SQL_FILE_NAME = "dataset_querys_akershus/fotrute_akershus.sql" # SQL file with a query to execute
+TABLE_NAME = "fotrute_akershus"
 SCHEMA = "public" # if None, default is public
 
 
