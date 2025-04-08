9fec558ea331c78c7fae0d5077f0e1ea3e71028a
Harri <harrishan.jegatheeswaran@kartverket.no>
2025-04-08 10:43:15 +0200

    local database Akershus

---
+++ dataset_querys_akershus/fotrute_akershus.sql
@@ -0,0 +1,77 @@
+WITH kommuner AS (
+                SELECT
+                    k.objid AS kommune_objid, 
+                    a.navn AS kommunenavn,
+                    k.omrade AS geom
+                FROM 
+                    administrative_enheter_kommuner.kommune k
+                JOIN 
+                    administrative_enheter_kommuner.administrativenhetnavn a
+                ON 
+                    k.lokalid = a.kommune_fk
+                WHERE 
+                    a.sprak = 'nor'
+            ),
+            -- Samler alle fotruter-segmenter og beregner lengden på senterlinjen
+            fotrute AS (
+                SELECT 
+					f.objid AS objid, 
+                    ST_Union(f.senterlinje) AS geom,
+					f.objtype AS objtype,
+                    f.lokalid AS lokalid,
+                    f.navnerom AS navnerom,
+                    f.versjonid AS versjonid,
+                    f.datafangstdato AS datafangstdato,
+                    f.oppdateringsdato AS oppdateringsdato,
+                    f.noyaktighet AS noyaktighet,
+                    f.opphav AS opphav,
+                    f.omradeid AS omradeid,
+                    f.informasjon AS informasjon,
+                    f.rutefolger AS rutefolger,
+                    f.malemetode AS malemetode
+					
+                FROM 
+                    old_tur_og_friluftsruter.fotrute f
+
+                GROUP BY 
+					f.objid,
+					f.objtype,
+                    f.lokalid,
+                    f.navnerom,
+                    f.versjonid,
+                    f.datafangstdato,
+                    f.oppdateringsdato,
+                    f.noyaktighet,
+                    f.opphav,
+                    f.omradeid,
+                    f.informasjon,
+                    f.rutefolger,
+                    f.malemetode
+					
+            )
+            SELECT 
+					f.objid,
+					f.geom,
+					f.objtype,
+					f.lokalid,
+					f.navnerom,
+					f.versjonid,
+					f.datafangstdato,
+					f.oppdateringsdato,
+					f.noyaktighet,
+					f.opphav,
+					f.omradeid,
+					f.informasjon,
+					f.rutefolger,
+					f.malemetode
+                    k.kommunenavn
+
+
+            FROM 
+                fotrute f
+            JOIN 
+                kommuner k 
+            ON 
+                ST_Intersects(f.geom, k.geom)
+            WHERE 
+                k.kommunenavn ILIKE 'Nannestad', 'Lunner', 'Jevnaker', 'Nittedal', 'Bærum', 'Nesodden', 'Asker', 'Frogn', 'Vestby', 'Ås', 'Nordre Follo', 'Aurskog-Høland', 'Enebakk', 'Lørenskog', 'Rælingen', 'Lillestrøm', 'Nes', 'Gjerdrum', 'Ullensaker', 'Eidsvoll', 'Hurdal'
